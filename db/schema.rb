b96b49ddfa0a70224d0d0d4040662911513005f4
josornoc <jose_osorno@hotmail.com>
2015-02-19 17:17:23 +0100

    form updated and migration done for avatar

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150218112846) do
+ActiveRecord::Schema.define(version: 20150219161146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
